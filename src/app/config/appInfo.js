813bbc4bc94c1673cd8deec011ab8840a83ff0c5
Aje Damilola <ajedamilola2005@gmail.com>
2024-12-27 23:44:05 +0100

    Switching to render

--- src/app/config/appInfo.js
+++ src/app/config/appInfo.js
@@ -2,8 +2,8 @@
 export const appInfo = {
   // learn more about this on https://supertokens.com/docs/thirdpartyemailpassword/appinfo
   appName: "Tyndall",
-  apiDomain: process.env.NODE_ENV == "development" ? "http://localhost:3000" : "https://tyndall.vercel.app",
-  websiteDomain: process.env.NODE_ENV == "development" ? "http://localhost:3000" : "https://tyndall.vercel.app",
+  apiDomain: process.env.NODE_ENV == "development" ? "http://localhost:3000" : "https://tyndall.onrender.com",
+  websiteDomain: process.env.NODE_ENV == "development" ? "http://localhost:3000" : "https://tyndall.onrender.com",
   apiBasePath: "/api/auth",
   websiteBasePath: "/auth"
 }
