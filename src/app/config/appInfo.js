19047599b94dc4755ebba27b5a3421e83fcd00bb
Aje Damilola <ajedamilola2005@gmail.com>
2024-12-27 21:12:31 +0100

    Trying Supertokens Fix

--- src/app/config/appInfo.js
+++ src/app/config/appInfo.js
@@ -2,8 +2,8 @@
 export const appInfo = {
   // learn more about this on https://supertokens.com/docs/thirdpartyemailpassword/appinfo
   appName: "Tyndall",
-  apiDomain: "http://localhost:3000",
-  websiteDomain: "http://localhost:3000",
+  apiDomain: process.env.NODE_ENV == "development" ? "http://localhost:3000" : "https://tyndall.vercel.app",
+  websiteDomain: process.env.NODE_ENV == "development" ? "http://localhost:3000" : "https://tyndall.vercel.app",
   apiBasePath: "/api/auth",
   websiteBasePath: "/auth"
 }
